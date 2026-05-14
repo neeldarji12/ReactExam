7877a3cacba6596e4512e9a60075bbe9052e415a
neeldarji12 <neeldarji1431@gmail.com>
2026-05-14 16:23:28 +0530

    Update studentReducer.js

--- src/Components/Redux/reducers/studentReducer.js
+++ src/Components/Redux/reducers/studentReducer.js
@@ -20,6 +20,30 @@ const studentReducer = (state = initialState, action) => {
                 students: [...state.students, action.payload]
             };
 
+        case "DELETE_STUDENT":
+
+            return {
+                ...state,
+                students: state.students.filter(
+                    (student) =>
+                        student.id !== action.payload
+                )
+            };
+
+        case "UPDATE_STUDENT":
+
+            return {
+                ...state,
+
+                students: state.students.map(
+                    (student) =>
+
+                        student.id === action.payload.id
+                            ? action.payload
+                            : student
+                )
+            };
+
         default:
             return state;
     }
